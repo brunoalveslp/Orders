3bdb79156acfdb380867aeee6c3584be46731528
Bruno Alves <brunoalveslp200597@gmail.com>
2026-03-28 22:15:44 -0300

    FEAT(Domain) - Criado entidade agragada de pedido, enum de status e result pattern

---
+++ Orders/Orders.Domain/Entities/Order.cs
@@ -0,0 +1,46 @@
+﻿using Orders.Domain.Common;
+using Orders.Domain.Enums;
+
+namespace Orders.Domain.Entities;
+
+public class Order
+{
+    public Guid Id { get; private set; }
+    public string NomeCliente { get; private set; }
+    public string Descricao { get; private set; }
+    public decimal Valor { get; private set; }
+    public OrderStatus Status { get; private set; }
+
+    private Order(string nomeCliente, string descricao, decimal valor)
+    {
+        NomeCliente = nomeCliente;
+        Descricao = descricao;
+        Valor = valor;
+    }
+
+    public static Result<Order> Create (string nomeCliente, string descricao, decimal valor)
+    {
+        if (string.IsNullOrWhiteSpace(nomeCliente))
+            return Result<Order>.Failure("O nome do cliente é obrigatório.");
+
+        if (valor <= 0)
+            return Result<Order>.Failure("O valor do pedido deve ser maior que zero.");
+
+        var order = new Order(nomeCliente, descricao, valor)
+        {
+            Id = Guid.NewGuid(),
+            Status = OrderStatus.Pendente
+        };
+
+        return Result<Order>.Success(order);
+    }
+
+    public Result Process()
+    {
+        if (Status != OrderStatus.Pendente)
+            return Result.Failure("O pedido já foi processado.");
+
+        Status = OrderStatus.Processado;
+        return Result.Success();
+    }
+}
---
+++ Orders/Orders.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace Orders.Domain.Enums;
+
+public enum OrderStatus
+{
+    Pendente = 0,
+    Processado = 1
+}
